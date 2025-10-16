6ee2923a11e934434cfc8b1140416d49c214f59b
lydiaamrouche <lydiaamrouche22@gmail.com>
2025-10-16 15:58:35 +0200

    corriger

--- devinette.py
+++ devinette.py
@@ -1,10 +1,18 @@
-print("Voici la devinette")
+print("Voici la devinette ")
 
 nombre_secret = 23
 
-nombre_utilisateur = int(input("Taper un nombre entre 0 et 100"))
-
-if nombre_utilisateur == nombre_secret:
-    print("Gagné")
-else:
-    print("Perdu")
+while True:
+    try:
+        nombre_utilisateur = int(input("Tapez un nombre entre 0 et 100 : "))
+        
+        if 0 <= nombre_utilisateur <= 100:
+            if nombre_utilisateur == nombre_secret:
+                print("Gagné ")
+                break
+            else:
+                print("Perdu  Essayez encore.")
+        else:
+            print("Le nombre doit être entre 0 et 100. Réessayez.")
+    except ValueError:
+        print("Entrée invalide. Veuillez taper un nombre.")
